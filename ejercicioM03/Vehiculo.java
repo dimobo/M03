ae604ef75be784877eb49dcb9408d9a1e5f3e32f
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-07-23 15:26:01 +0200

    M03 refactor

---
+++ ejercicioM03/Vehiculo.java
@@ -0,0 +1,26 @@
+package ejercicioM03;
+
+public class Vehiculo {
+	
+	private String matricula, marca, color;
+	private Rueda[] ejeDelantero, ejeTrasero;
+	
+	// Constructor por defecto del objeto coche
+	public Vehiculo() {
+		matricula="";
+		marca="";
+		color="";
+		ejeDelantero = null;
+		ejeTrasero = null;
+	}
+
+	private Rueda[] generarRuedas(String modelo, double diametro) {
+		Rueda[] eje = new Rueda[2];
+		
+		
+		
+		
+		return eje;
+	}
+	
+}
